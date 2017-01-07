6d99000d11f4ff5e1ca0b37f25d4319f4d1c9513
Roberto Bruggemann <roberto.bruggemann@gmail.com>
2017-01-07 15:44:20 +0000

    Add forgotten cassert.h header guard

--- src/cassert.h
+++ src/cassert.h
@@ -1,3 +1,5 @@
+#ifndef CASSERT_H_
+#define CASSERT_H_
 /**
  * Thanks to http://stackoverflow.com/users/68204/rberteig
  * http://stackoverflow.com/questions/807244/c-compiler-asserts-how-to-implement
@@ -31,3 +33,5 @@
 #define _impl_PASTE(a,b) a##b
 #define _impl_CASSERT_LINE(predicate, line, file) \
     typedef char _impl_PASTE(assertion_failed_##file##_,line)[2*!!(predicate)-1];
+
+#endif
